139cf20d720976898f6a2631f2620b4a0f7ee9ab
Shofiatul Ayu Anikaningrum <shofiaa060@gmail.com>
2023-04-10 14:42:19 +0700

    migrasi_iceCream

---
+++ app/Http/Controllers/IceCreamController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ice_Cream;
+use Illuminate\Http\Request;
+
+class IceCreamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ice_Cream  $ice_Cream
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ice_Cream $ice_Cream)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ice_Cream  $ice_Cream
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ice_Cream $ice_Cream)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ice_Cream  $ice_Cream
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ice_Cream $ice_Cream)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ice_Cream  $ice_Cream
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ice_Cream $ice_Cream)
+    {
+        //
+    }
+}
